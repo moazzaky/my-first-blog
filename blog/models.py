09f6cc102b02bc61de980b7462dac2a217f9d7fc
moaz <moaz_khairy@yahoo.com>
2018-01-21 22:01:24 +0200

    new template

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 
 class Post(models.Model):
@@ -17,4 +18,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
 
